5738ae8321997125e2fdf517033caeebf4e0a3be
Christian Findlay <a@a.com>
2020-04-08 18:24:30 +1000

    Start getting some of the data

--- Covid19DB/Covid19DB/Program.cs
+++ Covid19DB/Covid19DB/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -14,11 +16,38 @@ namespace Covid19DB
             {
                 //Get the date
                 var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
-                var tokens = fileNameWithoutExtension.Split('-').Select(t => int.Parse(t)).ToList();
-                var date = new DateTimeOffset(tokens[2], tokens[0], tokens[1], 0, 0, 0, default);
+                var numbers = fileNameWithoutExtension.Split('-').Select(t => int.Parse(t)).ToList();
+                var date = new DateTimeOffset(numbers[2], numbers[0], numbers[1], 0, 0, 0, default);
 
                 //Read the text
                 var text = File.ReadAllText(fileName);
+
+                var lines = text.Split("\r\n").ToList();
+
+                var headerNames = lines[0].Split(',').ToList();
+
+                var confirmedIndex = headerNames.IndexOf(nameof(RawModel.Confirmed));
+                var deathsIndex = headerNames.IndexOf(nameof(RawModel.Deaths));
+
+                var rowModels = new List<RawModel>();
+
+                for (var i = 1; i< lines.Count;i++)
+                {
+                    var tokens = lines[i].Split(',').ToList();
+
+                    if (tokens.Count != headerNames.Count) continue;
+
+                    var confirmedText = tokens[confirmedIndex];
+                    var deathsText = tokens[deathsIndex];
+
+                    if (string.IsNullOrEmpty(confirmedText) && string.IsNullOrEmpty(deathsText)) continue;
+
+                    var rawModel = new RawModel { Confirmed = int.Parse(confirmedText) };
+
+                    rowModels.Add(rawModel);
+                }
+
+
             }
         }
     }
---
+++ Covid19DB/Covid19DB/RawModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Covid19DB
+{
+    public class RawModel
+    {
+        public string Country_Region { get; set; }
+        public string Province_State { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Long_ { get; set; }
+        public int Confirmed { get; set; }
+        public string Deaths { get; set; }
+    }
+}
