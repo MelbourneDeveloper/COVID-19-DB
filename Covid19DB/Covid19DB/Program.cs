9e96273f0031206b1776e6717701591710f836f9
Christian Findlay <a@a.com>
2020-04-08 14:26:58 +1000

    Add the app

---
+++ Covid19DB/Covid19DB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Covid19DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
