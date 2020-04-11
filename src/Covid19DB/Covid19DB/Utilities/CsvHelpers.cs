789d85b23c7e971b392de1a3d6adaecb7ee89d3c
Christian Findlay <a@a.com>
2020-04-12 09:35:10 +1000

    Add class to write data out to CSV

---
+++ src/Covid19DB/Covid19DB/Utilities/CsvHelpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Covid19DB.Utilities
+{
+    public static class CsvHelpers
+    {
+        /// <summary>
+        /// Writes a collection of data out to a CSV file
+        /// </summary>
+        public static void ToCsv<T>(this IEnumerable<T> models, string filePath)
+        {
+            if (models == null) throw new ArgumentNullException(nameof(models));
+
+            using var fileStream = File.OpenWrite(filePath);
+
+            var headers = typeof(T).GetProperties();
+            var headerRow = string.Join(',', headers.Select(h => h.Name)) + "\r\n";
+
+            WriteText(fileStream, headerRow);
+
+            foreach (var row in models)
+            {
+                var rowText = string.Join(',', headers.Select(h =>
+                {
+                    var value = h.GetValue(row, null);
+                    return value?.ToString();
+                })) + "\r\n";
+                WriteText(fileStream, rowText);
+            }
+        }
+
+        private static void WriteText(FileStream fileStream, string headerRow)
+        {
+            var bytes = Encoding.UTF8.GetBytes(headerRow);
+            fileStream.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
