968cdbfba02531fb970f7700df782149f320bb5b
Christian Findlay <a@a.com>
2020-04-18 08:32:49 +1000

    Start breaking the code up in to something like a model

--- src/Covid19DBApp/MainPage.xaml.cs
+++ src/Covid19DBApp/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Covid19DB.Db;
+using Covid19DB.Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
@@ -11,8 +13,72 @@ using Windows.UI.Xaml.Controls.Maps;
 
 namespace Covid19DBApp
 {
+    public class LocationInformation : INotifyPropertyChanged
+    {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Fields
+        private int confirmed;
+        private int deaths;
+        private int recoveries;
+        #endregion
+
+        #region Public Properties
+        public Location Location;
+
+        public LocationInformation(Location location)
+        {
+            Location = location;
+        }
+
+        public int Confirmed
+        {
+            get => confirmed;
+            set
+            {
+                confirmed = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Confirmed)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Active)));
+            }
+        }
+
+        public int Deaths
+        {
+            get => deaths;
+            set
+            {
+                deaths = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Deaths)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Active)));
+            }
+        }
+
+        public int Recoveries
+        {
+            get => recoveries;
+            set
+            {
+                recoveries = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Recoveries)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Active)));
+            }
+        }
+
+        public int Active => Confirmed - (Deaths + Recoveries);
+        #endregion
+    }
+
+
     public class ViewModel
     {
+        //private 
+
+        public ViewModel(IEnumerable<Location> locations)
+        {
+
+        }
+
         public ObservableCollection<MapElement> MapElements { get; } = new ObservableCollection<MapElement>();
     }
 
@@ -22,11 +88,13 @@ namespace Covid19DBApp
         {
             InitializeComponent();
 
-            var viewModel = new ViewModel();
+            ViewModel viewModel = null;
 
             using (var covid19DbContext = new Covid19DbContext())
             {
-                var locations = covid19DbContext.Locations.Where(p => p.Province.Region.Name == "Australia").Include(d=>d.Province);
+                var locations = covid19DbContext.Locations.Where(p => p.Province.Region.Name == "Australia").Include(d => d.Province).ToList();
+
+                viewModel = new ViewModel(locations);
 
                 foreach (var location in locations)
                 {
