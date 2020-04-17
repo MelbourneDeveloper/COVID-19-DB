90a4d56ab29f66c4ab18692954c569dd96720460
Christian Findlay <a@a.com>
2020-04-17 17:39:24 +1000

    Get the counts working

--- src/Covid19DBApp/MainPage.xaml.cs
+++ src/Covid19DBApp/MainPage.xaml.cs
@@ -1,58 +1,47 @@
 ﻿using Covid19DB.Db;
-using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Covid19DBApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
 
             var locationMapElements = new List<MapElement>();
 
             using (var covid19DbContext = new Covid19DbContext())
             {
-                var locations = covid19DbContext.Locations.Where(p => p.Province.Region.Name == "Australia");
+                var locations = covid19DbContext.Locations.Where(p => p.Province.Region.Name == "Australia").Include(d=>d.Province);
 
                 foreach (var location in locations)
                 {
                     if (!location.Latitude.HasValue) continue;
 
-                    var snPosition = new BasicGeoposition 
-                    { 
-                        Latitude = (double)location.Latitude.Value, 
+                    var asdfasd = covid19DbContext.LocationDays.Where(ld => ld.Location.Id == location.Id).Sum(ld => ld.NewCases);
+
+                    var basicGeoposition = new BasicGeoposition
+                    {
+                        Latitude = (double)location.Latitude.Value,
                         Longitude = (double)location.Longitude.Value
                     };
 
-                    var snPoint = new Geopoint(snPosition);
+                    var Geopoint = new Geopoint(basicGeoposition);
 
                     var mapIcon = new MapIcon
                     {
-                        Location = snPoint,
+                        Location = Geopoint,
                         NormalizedAnchorPoint = new Point(0.5, 1.0),
                         ZIndex = 0,
-                        Title = location.Name
+                        Title = $"{location?.Province?.Name} - {asdfasd}"
                     };
 
                     locationMapElements.Add(mapIcon);
@@ -66,9 +55,6 @@ namespace Covid19DBApp
             };
 
             TheMapControl.Layers.Add(locationsLayer);
-
-            //TheMapControl.Center = snPoint;
-            TheMapControl.ZoomLevel = 14;
 
         }
     }
