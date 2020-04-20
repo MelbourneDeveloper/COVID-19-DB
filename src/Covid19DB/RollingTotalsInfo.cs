3f8a814c089d20f303023451ea758c5ee7d483bf
Christian Findlay <a@a.com>
2020-04-21 07:21:50 +1000

    Update from JSU Split a file out

---
+++ src/Covid19DB/RollingTotalsInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Covid19DB
+{
+    public class RollingTotalsInfo
+    {
+        public int? RollingTotal { get; set; }
+        public DateTimeOffset PreviousDate { get; set; }
+        public int PreviousDateRowNumber { get; set; }
+    }
+}
